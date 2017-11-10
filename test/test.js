30dd0b3e41a520879e619b7d361ba332e582066d
Martin Lindström <martin.lindstrom@tretton37.com>
2017-11-10 14:01:14 +0100

    Added initial unit testing
    - Added Mocha & Sinon dependencies
    - Added test for statements
    
    Fixes part of #2

---
+++ test/test.js
@@ -0,0 +1,6 @@
+import assert from "assert";
+import sinon from "sinon";
+import jspicl from "../build/jspicl";
+import { luaStatement, javascriptStatement } from "./test_data";
+
+it("should transpile statements", () => {});
