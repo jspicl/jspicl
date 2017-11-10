e78bfbe614d1c61fa204e89e2338a3b925a32c58
morkalork <maffelu@gmail.com>
2017-11-10 10:32:30 +0100

    Fixes #21, added a split string polyfill

--- tester.js
+++ tester.js
@@ -2,5 +2,8 @@ import jspicl from "./src";
 
 const code = `
 // Add your code here to test jspicl
+var foo = "Hello,Agron!";
+var bar = foo.split(",");
+console.log(bar);
 `;
 console.log(jspicl(code));
