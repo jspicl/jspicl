3cbe7cf546bfbf740a2c59ce8132fbdd9e04f4e2
Martin Walter <martin.walter@tretton37.com>
2017-11-10 11:00:59 +0100

    Implementation of Object.values

--- src/constants.js
+++ src/constants.js
@@ -12,8 +12,9 @@ export const generalPolyfills = {
   "Math.max": values => `max(${values})`,
   "Math.floor": value => `flr(${value})`,
   "Object.assign": values => `merge({${values}})`,
-  "console.log": ([argument]) => `print(${argument})`,
-  "Math.random": () => "rnd(1)"
+  "Object.values": values => `objectValues(${values})`,
+  "Math.random": () => "rnd(1)",
+  "console.log": ([argument]) => `print(${argument})`
 };
 
 export const arrayPolyfills = {
@@ -23,6 +24,16 @@ export const arrayPolyfills = {
 };
 
 export const polyfills = `
+function objectValues(source)
+  local values = {}
+  local i = 1
+  for key, value in pairs(source) do
+    values[i] = value
+    i = 1+i
+  end
+
+  return values
+end
 function merge(sources)
   local target = sources[1]
   del(sources, target)
