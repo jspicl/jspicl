54f99637b7461bfe48d2d7b2eddaf99de5f127c2
Agron Kabashi <agron.kabashi@tretton37.com>
2017-11-13 16:29:44 +0100

    Better check for existence of property

--- src/expressions/identifier.js
+++ src/expressions/identifier.js
@@ -1,10 +1,9 @@
 const specialCases = {
-  undefined: "nil",
-  toString: "toString" // Because of fucking misshaps this will have to do
+  undefined: "nil"
 };
 
 // http://esprima.readthedocs.io/en/latest/syntax-tree-format.html#identifier
 export const Identifier = ({ name, value }) => {
   const identifier = (value || name).replace(/\$/g, "_");
-  return specialCases[identifier] || identifier;
+  return specialCases.hasOwnProperty(identifier) && specialCases[identifier] || identifier;
 };
