2f15dfb05513cda435e97cd7249ae3bb2a13fe88
agronkabashi <agron@cerberussolutions.se>
2017-11-13 20:44:31 +0100

    Rename function to clarify purpose

--- src/expressions/logicalExpression.js
+++ src/expressions/logicalExpression.js
@@ -1,12 +1,12 @@
 import transpile from "../transpile";
 
-const decorateExpression = (type, operator, expression) => type === LogicalExpression.name && operator === "and" ? `(${expression})` : expression;
+const wrapWithParanthesesIfNeeded = (type, operator, expression) => type === LogicalExpression.name && operator === "and" ? `(${expression})` : expression; // eslint-disable-line no-use-before-define
 
 // http://esprima.readthedocs.io/en/latest/syntax-tree-format.html#logical-expression
 export const LogicalExpression = ({ operator, left, right }) => {
   const logicalOperator = operator === "||" ? "or" : "and";
-  const leftExpression = decorateExpression(left.type, logicalOperator, transpile(left));
-  const rightExpression = decorateExpression(right.type, logicalOperator, transpile(right));
+  const leftExpression = wrapWithParanthesesIfNeeded(left.type, logicalOperator, transpile(left));
+  const rightExpression = wrapWithParanthesesIfNeeded(right.type, logicalOperator, transpile(right));
 
   return `${leftExpression} ${logicalOperator} ${rightExpression}`;
 };
