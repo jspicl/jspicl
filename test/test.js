dd44985a1154ace731f3349d56dae26da4ddf796
agronkabashi <agron@cerberussolutions.se>
2018-02-02 10:16:51 +0100

    Added unit test

--- test/test.js
+++
@@ -1,6 +0,0 @@
-import assert from "assert";
-import sinon from "sinon";
-import jspicl from "../build/jspicl";
-import { luaStatement, javascriptStatement } from "./test_data";
-
-it("should transpile statements", () => {});
