1597d31c15495bb1f58df28f29f8a542a24dc985
agronkabashi <agron.kabashi@tretton37.com>
2018-04-14 22:52:49 +0200

    Explicit scope

--- src/declarations/variableDeclarator.js
+++ src/declarations/variableDeclarator.js
@@ -2,14 +2,14 @@ import { transpile } from "../transpile";
 import { normalizeName } from "../helpers";
 
 // http://esprima.readthedocs.io/en/latest/syntax-tree-format.html#variable-declaration
-export const VariableDeclarator = ({ id, init }, { variables }) => {
+export const VariableDeclarator = ({ id, init }, scope) => {
   const { name } = id;
   const normalizedName = normalizeName(name);
   const value = transpile(init) || "nil";
 
   // Store variable metadata in the scope
   // for accessibility
-  variables[name] = {
+  scope.variables[name] = {
     name: normalizedName
     // type:
   };
