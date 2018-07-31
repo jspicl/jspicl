a9cee2cc5c3f5e7f1054e9992a4eb87e19462d5f
Agron Kabashi <agron.kabashi@tretton37.com>
2018-07-31 11:19:39 +0200

    Added support for conditional expressions
    Closes #35

--- build/jspicl.js
+++ build/jspicl.js
@@ -361,20 +361,13 @@ const ClassBody = ({ body }) => {
   }`;
 };
 
-// import { transpile } from "../transpile";
-
 // http://esprima.readthedocs.io/en/latest/syntax-tree-format.html#conditional-expression
-const ConditionalExpression = (/* { test, consequent, alternate } */) => {
-  // const testExpression = transpile(test);
-  // const consequentPath = transpile(consequent);
-  // const alternatePath = transpile(alternate);
+const ConditionalExpression = ({ test, consequent, alternate }) => {
+  const testExpression = transpile(test);
+  const consequentPath = transpile(consequent);
+  const alternatePath = transpile(alternate);
 
-  // return `if (${testExpression}) then
-  //   ${consequentPath}
-  // else
-  //   ${alternatePath}
-  // end`;
-  throw new Error("Conditional expressions such as 'a ? b : c;' are not supported.");
+  return `(function () if ${testExpression} then return ${consequentPath} else return ${alternatePath} end end)()`;
 };
 
 // http://esprima.readthedocs.io/en/latest/syntax-tree-format.html#function-expression
--- src/expressions/conditionalExpression.js
+++ src/expressions/conditionalExpression.js
@@ -1,15 +1,10 @@
-// import { transpile } from "../transpile";
+import { transpile } from "../transpile";
 
 // http://esprima.readthedocs.io/en/latest/syntax-tree-format.html#conditional-expression
-export const ConditionalExpression = (/* { test, consequent, alternate } */) => {
-  // const testExpression = transpile(test);
-  // const consequentPath = transpile(consequent);
-  // const alternatePath = transpile(alternate);
+export const ConditionalExpression = ({ test, consequent, alternate }) => {
+  const testExpression = transpile(test);
+  const consequentPath = transpile(consequent);
+  const alternatePath = transpile(alternate);
 
-  // return `if (${testExpression}) then
-  //   ${consequentPath}
-  // else
-  //   ${alternatePath}
-  // end`;
-  throw new Error("Conditional expressions such as 'a ? b : c;' are not supported.");
+  return `(function () if ${testExpression} then return ${consequentPath} else return ${alternatePath} end end)()`;
 };
