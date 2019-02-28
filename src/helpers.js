658d0f7260411adbea61c054e78f678d9af9082f
Agron Kabashi <agronkabashi@windwalker.local>
2019-03-01 00:04:54 +0100

    Properly wrap IIFEs

--- src/helpers.js
+++ src/helpers.js
@@ -1,1 +1,4 @@
 export const normalizeName = name => name.toLowerCase().replace(/\$/g, "_");
+
+export const wrapWithParantheses = (condition, expression) =>
+  condition ? `(${expression})` : expression;
