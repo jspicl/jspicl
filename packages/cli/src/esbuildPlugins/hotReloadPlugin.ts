76ec62c46c18af48908f182bdda3db5ef114bc0a
Agron Kabashi <dadgron@gmail.com>
2026-02-13 00:51:31 +0100

    Enhance hotReloadPlugin with script injection for hot reloading

--- packages/cli/src/esbuildPlugins/hotReloadPlugin.ts
+++ packages/cli/src/esbuildPlugins/hotReloadPlugin.ts
@@ -1,6 +1,9 @@
 import type {Plugin, PluginBuild} from "esbuild";
+import path from "node:path";
+import fs from "node:fs";
 import {createPico8Launcher} from "../createPico8Launcher.js";
 import type {Config} from "../types.js";
+import {logToFile} from "../logging.js";
 
 type HotReloadPluginOptions = {
   config: Config;
@@ -15,8 +18,29 @@ export const hotReloadPlugin = (options: HotReloadPluginOptions): Plugin => ({
       options.config.pipeOutputToConsole
     );
 
-    build.onEnd(() => {
+    build.onEnd(async () => {
       const {output} = options;
+
+      // Brute force hotreload injection
+      const hotReloadScript = `
+local shouldreload = false
+function hotreload()
+  cartdata("${options.config.picoOptions?.cartDataId || "jspicl_hotreload"}")
+  shouldreload=peek(0x5eff - 3) == 1
+  if shouldreload then
+    poke(0x5eff - 3, 0)
+    load("${path.basename(output)}")
+  end
+end`;
+
+      const content = fs.readFileSync(output, "utf-8");
+
+      const newContent = content
+        .replace("__lua__", `__lua__\n${hotReloadScript}`)
+        .replace("function _draw()", `function _draw()\n  hotreload()`);
+
+      logToFile(newContent, output);
+
       runPico(output);
     });
   }
